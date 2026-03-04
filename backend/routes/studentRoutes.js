3a442cabf8d635dc3906dfda6a95e46c2a32d82e
Santhosh1507 <webdesign1507@gmail.com>
2026-03-04 14:22:56 +0530

    backend create

--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const studentController = require("../controllers/studentController");
+const upload = require("../config/image");
 
-router.post("/register", studentController.registerStudent);
+router.post("/register", upload.single("image"), studentController.registerStudent);
 router.get("/students", studentController.getStudents);
 
 module.exports = router;
